6da5d6bc90ad91a2a10f88237123aab0ae5fea25
unknown <lennyfloresalave@gmail.com>
2024-08-28 19:38:17 -0300

    api

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import Router from './src/Routes';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import PokemonListScreen from './screens/PokemonListScreen';
+import PokemonDetailScreen from './screens/PokemonDetailScreen';
 
-export default function App() {
-  return(
-    <Router/>
-  )
-}
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="PokemonList">
+        <Stack.Screen name="PokemonList" component={PokemonListScreen} options={{ title: 'Lista de Pokémon' }} />
+        <Stack.Screen name="PokemonDetail" component={PokemonDetailScreen} options={{ title: 'Detalhes do Pokémon' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
